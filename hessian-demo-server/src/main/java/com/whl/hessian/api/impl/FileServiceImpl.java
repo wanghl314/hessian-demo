6c516e25bfe0df833170496a87dc97ca63c60cc5
王洪亮 <w505957125@aliyun.com>
2023-11-10 10:59:37 +0800

    1. remove commons-io:commons-io 2. replace org.apache.commons.io.FileUtils with java nio api

--- hessian-demo-server/src/main/java/com/whl/hessian/api/impl/FileServiceImpl.java
+++ hessian-demo-server/src/main/java/com/whl/hessian/api/impl/FileServiceImpl.java
@@ -1,14 +1,15 @@
 package com.whl.hessian.api.impl;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
 import javax.servlet.annotation.WebServlet;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.caucho.hessian.server.HessianServlet;
@@ -20,31 +21,35 @@ public class FileServiceImpl extends HessianServlet implements FileService {
 
     @Override
     public String upload(String name, InputStream is) throws IOException {
-        final String PATH = this.getFileStoreDirectory();
         String suffix = "";
 
         if (StringUtils.contains(name, ".")) {
             suffix = name.substring(name.lastIndexOf("."));
         }
         String storeFileName = UUID.randomUUID() + suffix;
-        File destination = new File(PATH + File.separator + storeFileName);
-        FileUtils.copyInputStreamToFile(is, destination);
+        Path path = this.getFileStorePath();
+        Path storePath = path.resolve(storeFileName);
+
+        if (!Files.exists(storePath.getParent())) {
+            Files.createDirectories(storePath.getParent());
+        }
+        Files.copy(is, storePath, StandardCopyOption.REPLACE_EXISTING);
         return storeFileName;
     }
 
     @Override
     public InputStream download(String name) throws IOException {
-        final String PATH = this.getFileStoreDirectory();
-        File file = new File(PATH + File.separator + name);
+        Path path = this.getFileStorePath();
+        Path file = path.resolve(name);
 
-        if (file.exists()) {
-            return Files.newInputStream(file.toPath());
+        if (Files.exists(file)) {
+            return Files.newInputStream(file);
         }
         return null;
     }
 
-    private String getFileStoreDirectory() {
-        return System.getProperty("user.dir") + File.separator + "upload";
+    private Path getFileStorePath() {
+        return Paths.get(System.getProperty("user.dir"), "upload");
     }
 
 }
